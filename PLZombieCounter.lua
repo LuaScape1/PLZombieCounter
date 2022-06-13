cc3e5da1f44682dc373b31b883a8faa156b561b5
LuaScape1 <70647100+LuaScape1@users.noreply.github.com>
2022-06-13 14:17:05 -0400

    Update PLZombieCounter.lua

--- PLZombieCounter.lua
+++ PLZombieCounter.lua
@@ -1,8 +1,13 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
 
 local counter = Instance.new("ScreenGui")
 local TextLabel = Instance.new("TextLabel")
-local UiStroke = Instance.new("UIStroke")
-local UICorner = Instance.new("UICorner")
+local uistroke = Instance.new("UIStroke")
+
+--Properties:
 
 counter.Name = "counter"
 counter.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
@@ -11,30 +16,26 @@ counter.ResetOnSpawn = false
 
 TextLabel.Parent = counter
 TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BackgroundTransparency = 0.500
-TextLabel.Position = UDim2.new(0.41548124, 0, 0.0178325977, 0)
-TextLabel.Size = UDim2.new(0.168298453, 0, 0.0406091399, 0)
-TextLabel.Font = Enum.Font.GothamBold
-TextLabel.Text = "Zombies Left:"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Position = UDim2.new(0.02, 0,0.686, 0)
+TextLabel.Size = UDim2.new(0.0785386488, 0, 0.0733122677, 0)
+TextLabel.Font = Enum.Font.Gotham
+TextLabel.Text = "0"
+TextLabel.TextColor3 = Color3.fromRGB(216,216,216)
+TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
 
 
-TextLabel.RichText = true
+uistroke.Parent = TextLabel
+uistroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Contextual
+uistroke.Color = Color3.new(0,0,0)
+uistroke.Thickness = 1.1
 
+-- Scripts:
 
-
-UiStroke.Parent = TextLabel
-UiStroke.Color = Color3.new(255,255,255)
-UiStroke.Thickness = 2.2
-UiStroke.Transparency = 0.5
-UiStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-
-UICorner.Parent = TextLabel
-
-
-local function PGMN_fake_script() -- TextLabel.LocalScript 
+local function FIAZFI_fake_script() -- TextLabel.LocalScript 
 	local script = Instance.new('LocalScript', TextLabel)
 
 	local uis = game:GetService("UserInputService")
@@ -52,7 +53,7 @@ local function PGMN_fake_script() -- TextLabel.LocalScript
 	
 	
 	while wait() do
-		script.Parent.Text=[[Zombies Left: <font color="rgb(255, 0, 0)">]]..#game.Workspace.Baddies:GetChildren()..[[</font>]]
+		script.Parent.Text = #game.Workspace.Baddies:GetChildren()
 	end
 end
-coroutine.wrap(PGMN_fake_script)()
+coroutine.wrap(FIAZFI_fake_script)()
